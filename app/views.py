45e5245cd525ff0e3854c6eb49f4246e04289056
neo1218 <neo1218@yeah.net>
2016-11-03 09:45:16 +0800

    update sql

--- app/views.py
+++ app/views.py
@@ -6,14 +6,22 @@ from app import app, db, basedir
 @app.route('/result/',  methods=['GET'])
 def result():
     alldata_encode = []
-    sql = "SELECT table1.Content1,table2.Content2, table3.Content3 FROM table1, table2, table3 ORDER BY rand() limit 1"
+    # sql1 = "SELECT table1.Content1,table2.Content2, table3.Content3 FROM table1, table2, table3 ORDER BY rand() limit 1"
+    sql1 = "SELECT Content1 from table1 ORDER BY rand() limit 1"
+    sql2 = "SELECT Content2 from table2 ORDER BY rand() limit 1"
+    sql3 = "SELECT Content3 from table3 ORDER BY rand() limit 1"
     cursor = db.cursor()
-    r=cursor.execute(sql)
-    alldata = cursor.fetchall()
-    # for datas in alldata:
-    #     for data in datas:
-    #         alldata_encode.append(data.decode('utf-8'))
+    cursor.execute(sql1); r1 = cursor.fetchall()
+    r2=cursor.execute(sql2); r2 = cursor.fetchall()
+    r3=cursor.execute(sql3); r3 = cursor.fetchall()
+    alldata = r1[0] + r2[0] + r3[0]
+    for data in alldata:
+        try:
+            alldata_encode.append(data.decode('utf-8'))
+        except UnicodeDecodeError:
+            pass
     #db.query = sql
     return jsonify({
-        'result':alldata
+        # 'result':alldata_encode
+	'result': alldata_encode[0] + alldata_encode[1] + alldata_encode[2]
     })
