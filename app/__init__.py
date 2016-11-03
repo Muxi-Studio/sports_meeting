de350185dd604168e7af4b4eb8610f591554a79d
neo1218 <neo1218@yeah.net>
2016-11-03 09:06:06 +0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,17 @@
 # coding: utf-8
 
+import MySQLdb
+import os
 from flask import Flask
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key is here'
 
+HOST = os.environ.get("SPORTS_DB_URL")
+USER = os.environ.get("SPORTS_DB_USER")
+PASSWD = os.environ.get("SPORTS_DB_PASS")
+DB = os.environ.get("SPORTS_DB_NAME")
+db = MySQLdb.connect(host=HOST,user=USER,passwd=PASSWD,db=DB)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 from . import views, forms
--- app/views.py
+++ app/views.py
@@ -1,29 +1,19 @@
 # coding: utf-8
 
 from flask import render_template,Flask,jsonify
-import MySQLdb
-import os, sys, string
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-app = Flask(__name__)
-HOST = os.environ.get("HOST")
-USER = os.environ.get("USER")
-PASSWD = os.environ.get("PASSWD")
-DB = os.environ.get("DB")
-db = MySQLdb.connect(host=HOST,user=USER,passwd=PASSWD,db=DB)
-
+from app import app, db, basedir
     
 @app.route('/result/',  methods=['GET'])
 def result():
+    alldata_encode = []
     sql = "SELECT table1.Content1,table2.Content2, table3.Content3 FROM table1, table2, table3 ORDER BY rand() limit 1"
     cursor = db.cursor()
     r=cursor.execute(sql)
     alldata = cursor.fetchall()
-    db.query = sql
+    # for datas in alldata:
+    #     for data in datas:
+    #         alldata_encode.append(data.decode('utf-8'))
+    #db.query = sql
     return jsonify({
         'result':alldata
     })
-
-if __name__ == '__main__':
-	app.run(debug=True)
-
